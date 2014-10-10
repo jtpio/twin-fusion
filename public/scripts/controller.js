96d9b8602d85697bcd6d0776f8bb843ca4b65146
Husenap <Hussein_t_97@hotmail.com>
2014-10-10 15:29:23 +0200

    Fixed some problems with the Controller
    Fixed the resizing and texture changing, and also fixed the indentation
    of controller.js and aurora.css

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -1,70 +1,70 @@
 'use strict';
 
 require.config({
-  shim: {
-    'socketio': {
-      exports: 'io'
+    shim: {
+        'socketio': {
+            exports: 'io'
+        }
+    },
+    paths: {
+        socketio: 'https://cdn.socket.io/socket.io-1.0.6'
     }
-  },
-  paths: {
-    socketio: 'https://cdn.socket.io/socket.io-1.0.6'
-  }
 });
 
 var main = function(GameClient) {
     var gameClient;
 
     var Game = (function () {
-      function Game (gameControls, sound) {
-        this.sound = sound;
-        this.gameControls = gameControls;
-        this.gameControls.sound = sound;
-        this.speechRecognition = null;
-      }
+        function Game (gameControls, sound) {
+            this.sound = sound;
+            this.gameControls = gameControls;
+            this.gameControls.sound = sound;
+            this.speechRecognition = null;
+        }
 
-      Game.prototype.initialize = function () {
-          this.gameControls.attachGameControls();
-      };
+        Game.prototype.initialize = function () {
+            this.gameControls.attachGameControls();
+        };
 
-      Game.prototype.addListener = function (listener, callback) {
-          gameClient.addEventListener(listener, callback);
-      };
+        Game.prototype.addListener = function (listener, callback) {
+            gameClient.addEventListener(listener, callback);
+        };
 
-      Game.prototype.end = function (data) {
-          var winnerBackgroundColor = data.color.toString(16);
-          $('body').css('backgroundColor', '#' + winnerBackgroundColor);
-          $('body').css('margin', 0);
-          $('body').css('background-image', 'none');
-          $('#outerGamePad').hide();
-          $('#victoryPannel').fadeIn();
-          document.getElementById('victoryPannel').style.display = 'flex';
-          newSound().startSound('assets/winning.mp3');
-      };
+        Game.prototype.end = function (data) {
+            var winnerBackgroundColor = data.color.toString(16);
+            $('body').css('backgroundColor', '#' + winnerBackgroundColor);
+            $('body').css('margin', 0);
+            $('body').css('background-image', 'none');
+            $('#outerGamePad').hide();
+            $('#victoryPannel').fadeIn();
+            document.getElementById('victoryPannel').style.display = 'flex';
+            new Sound().startSound('assets/winning.mp3');
+        };
 
-      Game.prototype.start = function () {
-        this.initialize();
+        Game.prototype.start = function () {
+            this.initialize();
 
-        gameClient = new GameClient('player');
+            gameClient = new GameClient('player');
 
-        $('#outerGamePad').hide();
+            $('#outerGamePad').hide();
 
-        this.addListener ('connect', function () {
-          $('body').css('backgroundColor', '');
-          $('body').css('background-image', 'url(../assets/img/background.png)');
-          $('#victoryPannel').hide();
-          $('#loadingPannel').hide();
-          $('#gameIDInput').fadeOut();
-          $('#outerGamePad').fadeIn();
-          console.log ('player connected');
-        });
+            this.addListener ('connect', function () {
+                $('body').css('backgroundColor', '');
+                $('body').css('background-image', 'url(../assets/img/background.png)');
+                $('#victoryPannel').hide();
+                $('#loadingPannel').hide();
+                $('#gameIDInput').fadeOut();
+                $('#outerGamePad').fadeIn();
+                console.log ('player connected');
+            });
 
-        this.addListener('winner', this.end);
+            this.addListener('winner', this.end);
 
-        this.addListener('disconnect', function () {
-            alert ('You cannot connect. Please try next game :)');
-        });
-      };
-      return Game;
+            this.addListener('disconnect', function () {
+                alert ('You cannot connect. Please try next game :)');
+            });
+        };
+        return Game;
     })();
      //=============================================== PIXI.JS CONTROLLER ===============================================
     var stage = new PIXI.Stage(0x000000, true);
@@ -82,6 +82,7 @@ var main = function(GameClient) {
         pad = PIXI.Texture.fromImage('assets/img/gamePad.png'),
         buttons = [], dx = [0, 0, -1, 1], dy = [-1, 1, 0, 0],
         dir;
+    var sx = window.innerWidth, sy = window.innerHeight, dsx, dsy;
 
     var Button = function(x, y, a, ap, isStatic, dir, sound){
         var button = new PIXI.Sprite(a);
@@ -91,31 +92,61 @@ var main = function(GameClient) {
         button.position.y = y;
         button.setInteractive(true);
 
-        button.mousedown = button.touchstart = function(data){
+        button.mousedown = function(data){
             this.isdown = true;
             this.setTexture(ap);
-            this.alpha = 1;
-            if(!isStatic){this.width = this.isOver?90:100; this.height = this.isOver?90:100;}
-            if(!this.isOver)this.setTexture(a);
+            if(!isStatic){
+                button.scale.x = sx/window.innerWidth;
+                button.scale.y = sy/window.innerHeight;
+                sound.startSound('assets/button.mp3');
+            }
+        }
+        button.touchstart = function(data){
+            this.isdown = true;
+            this.setTexture(ap);
+            if(!isStatic){
+                button.scale.x = sx/window.innerWidth - 0.1*(sx/window.innerWidth);
+                button.scale.y = sy/window.innerHeight - 0.1*(sy/window.innerHeight);
+            }
         }    
-        button.mouseup = button.touchend = function(data){
+        button.mouseup = function(data){
             this.isdown = false;
-            this.setTexture(a);
-            if(!isStatic){this.width = this.isOver?100:90; this.height = this.isOver?100:90;}
+            this.setTexture(a);            
+            if(!isStatic){
+                button.scale.x = sx/window.innerWidth + 0.1*(sx/window.innerWidth);
+                button.scale.y = sy/window.innerHeight + 0.1*(sy/window.innerHeight);
+            }
+        }
+        button.touchend = function(data){
+            this.isdown = false;
+            this.setTexture(a);            
+            if(!isStatic){
+                button.scale.x = sx/window.innerWidth;
+                button.scale.y = sy/window.innerHeight;
+            }
         }
         button.mouseover = function(data){
             this.isOver = true;
-            if(this.isdown)return;
             this.setTexture(a);
-            if(!isStatic){this.width = this.isOver?100:90; this.height = this.isOver?100:90;}
+            if(!isStatic){
+                button.scale.x = sx/window.innerWidth + 0.1*(sx/window.innerWidth);
+                button.scale.y = sy/window.innerHeight + 0.1*(sy/window.innerHeight);
+            }
         }
         button.mouseout = function(data){
             this.isOver = false;
             this.setTexture(a);
             if(!isStatic){
-                this.width = this.isOver?100:90; this.height = this.isOver?100:90;
-                if(this.isdown)sound.startSound('assets/button.mp3');
+                button.scale.x = sx/window.innerWidth;
+                button.scale.y = sy/window.innerHeight;
             }
+        }
+        button.mouseupoutside = button.touchendoutside = function(data){
+            this.isOver = false;
+            this.isdown = false;
+            this.setTexture(a);
+            button.scale.x = sx/window.innerWidth;
+            button.scale.y = sy/window.innerHeight;
         }
         button.click = button.tap = function(data){
             if(!isStatic){
@@ -125,8 +156,6 @@ var main = function(GameClient) {
         }
         return button;
     }
-    var sx = window.innerWidth;
-    var sy = window.innerHeight;
     buttons.push(Button(sx/2, sy/2, pad, pad, true, null, new Sound()));
     buttons.push(Button(sx/2, sy/2 - 100, ua, uap, false, 0, new Sound()));
     buttons.push(Button(sx/2, sy/2 + 100, da, dap, false, 1, new Sound()));
@@ -135,19 +164,20 @@ var main = function(GameClient) {
 
     for(var i = 0; i < buttons.length; i++)stage.addChild(buttons[i]);
 
-    
     requestAnimFrame(animate);
     function animate(){
-        stage.width = window.innerWidth;
-        stage.height = window.innerHeight;
         requestAnimFrame(animate);
-        for(var i = 0; i < buttons.length; i++){
-            buttons[i].scale.x = sx/window.innerWidth;
-            buttons[i].scale.y = sy/window.innerHeight;
-            if(i > 0){
-                buttons[i].position.x = sx/2+100*(sx/window.innerWidth)*dx[i-1];
-                buttons[i].position.y = sy/2+100*(sy/window.innerHeight)*dy[i-1];
+        if(dsx != window.innerWidth || dsy != window.innerHeight){
+            for(var i = 0; i < buttons.length; i++){
+                buttons[i].scale.x = sx/window.innerWidth;
+                buttons[i].scale.y = sy/window.innerHeight;
+                if(i > 0){
+                    buttons[i].position.x = sx/2+100*(sx/window.innerWidth)*dx[i-1];
+                    buttons[i].position.y = sy/2+100*(sy/window.innerHeight)*dy[i-1];
+                }
             }
+            dsx = window.innerWidth;
+            dsy = window.innerHeight;
         }
         renderer.render(stage);
     }
@@ -156,10 +186,9 @@ var main = function(GameClient) {
         function GameControls() {}
         GameControls.prototype.attachGameControls = function () {
             $('#joinGame').click(function (e) {
-              gameClient.sendCmd('gameID', { gameID: $('#gameID').val().toUpperCase() });
+                gameClient.sendCmd('gameID', { gameID: $('#gameID').val().toUpperCase() });
             });
         };
-
         return GameControls;
     })();
 
@@ -169,5 +198,5 @@ var main = function(GameClient) {
 
 // Start the main app logic.
 requirejs([
-  './gameClient'
+    './gameClient'
 ], main);
